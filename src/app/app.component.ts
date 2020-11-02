9be0d15b4952ff4cf73bb998ab153eb88a1cbb0a
mouadziani <mouad.ziani@kobail.fr>
2020-11-02 20:04:41 +0100

    feat(counter): added simple counter managed with ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {select, State, Store} from '@ngrx/store';
+import { increment, decrement, reset } from './store/app.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ngrx-practices';
+export class AppComponent implements OnInit {
+
+  counter = 0;
+
+  constructor(public state: Store<{ count: number }>) {}
+
+  ngOnInit(): void {
+    this.state.pipe(select('count'))
+        .subscribe((count) => {
+          this.counter = count;
+        });
+  }
+
+  increment(): void {
+    this.state.dispatch(increment());
+  }
+
+  decrement(): void {
+    this.state.dispatch(decrement());
+  }
+
+  reset(): void {
+    this.state.dispatch(reset());
+  }
 }
